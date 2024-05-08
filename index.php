830f3fd73f6da64be2845db2a1c5a96c0506cdd9
Роман Карнаухов <WorkKarn@yandex.ru>
2024-05-08 10:43:39 +0700

    Интеграция вставных областей "О нас"

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetTitle('Главная');
 $APPLICATION->SetPageProperty('TITLE', "Главная");
 ?> 
+//Слайдер
 <?$APPLICATION->IncludeComponent("bitrix:news.list", "slider_main_page", Array(
 	"ACTIVE_DATE_FORMAT" => "d.m.Y",	// Формат показа даты
 		"ADD_SECTIONS_CHAIN" => "N",	// Включать раздел в цепочку навигации
@@ -67,6 +68,58 @@ $APPLICATION->SetPageProperty('TITLE', "Главная");
 	),
 	false
 );?>
+// О нас
+<section class="who-area-are pad-90" id="about_us">
+	<div class="container">
+		<h2 class="title-1">
+			<? $APPLICATION->IncludeComponent(
+				"bitrix:main.include",
+				"",
+				array(
+					"AREA_FILE_RECURSIVE" => "Y",
+					"AREA_FILE_SHOW" => "file",
+					"AREA_FILE_SUFFIX" => "inc",
+					"EDIT_TEMPLATE" => "standard.php",
+					"PATH" => SITE_TEMPLATE_PATH . "/include_file/main/titel.php"
+				)
+			); ?>
+		</h2>
+		<div class="row">
+			<div class="col-md-7">
+				<div class="who-we">
+				<? $APPLICATION->IncludeComponent(
+				"bitrix:main.include",
+				"",
+				array(
+					"AREA_FILE_RECURSIVE" => "Y",
+					"AREA_FILE_SHOW" => "file",
+					"AREA_FILE_SUFFIX" => "inc",
+					"EDIT_TEMPLATE" => "standard.php",
+					"PATH" => SITE_TEMPLATE_PATH . "/include_file/main/text.php"
+				)
+			); ?>
+					
+				</div>
+			</div>
+			<div class="col-md-5">
+				<div class="about-bg">
+				<? $APPLICATION->IncludeComponent(
+				"bitrix:main.include",
+				"",
+				array(
+					"AREA_FILE_RECURSIVE" => "Y",
+					"AREA_FILE_SHOW" => "file",
+					"AREA_FILE_SUFFIX" => "inc",
+					"EDIT_TEMPLATE" => "standard.php",
+					"PATH" => SITE_TEMPLATE_PATH . "/include_file/main/img.php"
+				)
+			); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
