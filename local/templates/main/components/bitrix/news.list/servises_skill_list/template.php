726254fe5cb3406b6a3047c1162dd4a452a767ee
Роман Карнаухов <WorkKarn@yandex.ru>
2024-05-09 13:00:34 +0700

    Создание включаемых облосчтей и листа с процентами скилов

--- local/templates/main/components/bitrix/news.list/servises_skill_list/template.php
+++ local/templates/main/components/bitrix/news.list/servises_skill_list/template.php
@@ -1,4 +1,4 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 /** @var array $arParams */
 /** @var array $arResult */
 /** @global CMain $APPLICATION */
@@ -12,75 +12,24 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<div class="news-list">
-<?if($arParams["DISPLAY_TOP_PAGER"]):?>
-	<?=$arResult["NAV_STRING"]?><br />
-<?endif;?>
-<?foreach($arResult["ITEMS"] as $arItem):?>
-	<?
-	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-	?>
-	<p class="news-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-				<a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><img
-						class="preview_picture"
-						border="0"
-						src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-						width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
-						height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
-						alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-						title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-						style="float:left"
-						/></a>
-			<?else:?>
-				<img
-					class="preview_picture"
-					border="0"
-					src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-					width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
-					height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
-					alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-					title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-					style="float:left"
-					/>
-			<?endif;?>
-		<?endif?>
-		<?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
-			<span class="news-date-time"><?echo $arItem["DISPLAY_ACTIVE_FROM"]?></span>
-		<?endif?>
-		<?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
-			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-				<a href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><b><?echo $arItem["NAME"]?></b></a><br />
-			<?else:?>
-				<b><?echo $arItem["NAME"]?></b><br />
-			<?endif;?>
-		<?endif;?>
-		<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
-			<?echo $arItem["PREVIEW_TEXT"];?>
-		<?endif;?>
-		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-			<div style="clear:both"></div>
-		<?endif?>
-		<?foreach($arItem["FIELDS"] as $code=>$value):?>
-			<small>
-			<?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?>
-			</small><br />
-		<?endforeach;?>
-		<?foreach($arItem["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
-			<small>
-			<?=$arProperty["NAME"]?>:&nbsp;
-			<?if(is_array($arProperty["DISPLAY_VALUE"])):?>
-				<?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?>
-			<?else:?>
-				<?=$arProperty["DISPLAY_VALUE"];?>
-			<?endif?>
-			</small><br />
-		<?endforeach;?>
-	</p>
-<?endforeach;?>
-<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
-	<br /><?=$arResult["NAV_STRING"]?>
-<?endif;?>
-</div>
+
+
+
+<? if (!empty($arResult)) : ?>
+	<div class="progress-list">
+		<? foreach ($arResult["ITEMS"] as $arItem) : ?>
+			<?
+			$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+			$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+			?>
+			<div class="progress" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
+				<div class="lead">
+					<?= isset($arItem["NAME"]) ? $arItem["NAME"] : "" ?>
+				</div>
+				<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style='width: <?= isset($arItem["PREVIEW_TEXT"]) ? $arItem["PREVIEW_TEXT"] : "0%" ?>'>
+					<span><?= isset($arItem["PREVIEW_TEXT"]) ? $arItem["PREVIEW_TEXT"] : "0%" ?></span>
+				</div>
+			</div>
+		<? endforeach ?>
+	</div>
+<? endif ?>
--- test.php
+++ test.php
@@ -3,168 +3,130 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("test");
 $APPLICATION->SetPageProperty('TITLE', "Тест");
 ?><section class="who-area-are bg-color pad-90">
-<div class="container">
-	<div class="row">
-		<div class="col-lg-6 col-md-6">
-			<div class="who-we">
-				<h2 class="title-1"><?$APPLICATION->IncludeComponent(
-	"bitrix:main.include",
-	"",
-	Array(
-		"AREA_FILE_RECURSIVE" => "Y",
-		"AREA_FILE_SHOW" => "file",
-		"AREA_FILE_SUFFIX" => "inc",
-		"EDIT_TEMPLATE" => "standard.php",
-		"PATH" => SITE_TEMPLATE_PATH."/include_file/servises/titel.php"
-	)
-);?></h2>
-				<p>
-					 <?$APPLICATION->IncludeComponent(
-	"bitrix:main.include",
-	"",
-	Array(
-		"AREA_FILE_RECURSIVE" => "Y",
-		"AREA_FILE_SHOW" => "file",
-		"AREA_FILE_SUFFIX" => "inc",
-		"EDIT_TEMPLATE" => "standard.php",
-		"PATH" => SITE_TEMPLATE_PATH."/include_file/servises/text1.php"
-	)
-);?>
-				</p>
-				<p>
-					 <?$APPLICATION->IncludeComponent(
-	"bitrix:main.include",
-	"",
-	Array(
-		"AREA_FILE_RECURSIVE" => "Y",
-		"AREA_FILE_SHOW" => "file",
-		"AREA_FILE_SUFFIX" => "inc",
-		"EDIT_TEMPLATE" => "standard.php",
-		"PATH" => SITE_TEMPLATE_PATH."/include_file/servises/text2.php"
-	)
-);?>
-				</p>
-			</div>
-		</div>
-		<div class="col-lg-6 col-md-6">
-			<div class="our-skill">
-				<h2 class="title-1"><?$APPLICATION->IncludeComponent(
-	"bitrix:main.include",
-	"",
-	Array(
-		"AREA_FILE_RECURSIVE" => "Y",
-		"AREA_FILE_SHOW" => "file",
-		"AREA_FILE_SUFFIX" => "inc",
-		"EDIT_TEMPLATE" => "standard.php",
-		"PATH" => SITE_TEMPLATE_PATH."/include_file/servises/name_skill.php"
-	)
-);?></h2>
-				<div class="progress-list">
-					<div class="progress">
-						<div class="lead">
-							 Web-дизайн
-						</div>
-						<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 90%;">
-							 90%
-						</div>
-					</div>
-					<div class="progress">
-						<div class="lead">
-							 PHP (Bitrix / Laravel / Symfony)
-						</div>
-						<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 80%;">
-							 80%
-						</div>
-					</div>
-					<div class="progress">
-						<div class="lead">
-							 Node.js
-						</div>
-						<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
-							 75%
-						</div>
-					</div>
-					<div class="progress">
-						<div class="lead">
-							 JavaScript (Angular / React)
-						</div>
-						<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
-							 75%
-						</div>
-					</div>
-					<div class="progress">
-						<div class="lead">
-							 CSS (LESS)
-						</div>
-						<div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;">
-							 80%
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-<?$APPLICATION->IncludeComponent("bitrix:news.list", "servises_skill_list", Array(
-	"ACTIVE_DATE_FORMAT" => "d.m.Y",	// Формат показа даты
-		"ADD_SECTIONS_CHAIN" => "N",	// Включать раздел в цепочку навигации
-		"AJAX_MODE" => "N",	// Включить режим AJAX
-		"AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
-		"AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
-		"AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
-		"AJAX_OPTION_STYLE" => "N",	// Включить подгрузку стилей
-		"CACHE_FILTER" => "N",	// Кешировать при установленном фильтре
-		"CACHE_GROUPS" => "N",	// Учитывать права доступа
-		"CACHE_TIME" => "36000000",	// Время кеширования (сек.)
-		"CACHE_TYPE" => "N",	// Тип кеширования
-		"CHECK_DATES" => "Y",	// Показывать только активные на данный момент элементы
-		"DETAIL_URL" => "",	// URL страницы детального просмотра (по умолчанию - из настроек инфоблока)
-		"DISPLAY_BOTTOM_PAGER" => "N",	// Выводить под списком
-		"DISPLAY_DATE" => "N",	// Выводить дату элемента
-		"DISPLAY_NAME" => "Y",	// Выводить название элемента
-		"DISPLAY_PICTURE" => "N",	// Выводить изображение для анонса
-		"DISPLAY_PREVIEW_TEXT" => "Y",	// Выводить текст анонса
-		"DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
-		"FIELD_CODE" => array(	// Поля
-			0 => "NAME",
-			1 => "PREVIEW_TEXT",
-			2 => "",
-		),
-		"FILTER_NAME" => "",	// Фильтр
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",	// Скрывать ссылку, если нет детального описания
-		"IBLOCK_ID" => "7",	// Код информационного блока
-		"IBLOCK_TYPE" => "content",	// Тип информационного блока (используется только для проверки)
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",	// Включать инфоблок в цепочку навигации
-		"INCLUDE_SUBSECTIONS" => "N",	// Показывать элементы подразделов раздела
-		"MESSAGE_404" => "",	// Сообщение для показа (по умолчанию из компонента)
-		"NEWS_COUNT" => "5",	// Количество новостей на странице
-		"PAGER_BASE_LINK_ENABLE" => "N",	// Включить обработку ссылок
-		"PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",	// Время кеширования страниц для обратной навигации
-		"PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
-		"PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
-		"PAGER_TEMPLATE" => ".default",	// Шаблон постраничной навигации
-		"PAGER_TITLE" => "Новости",	// Название категорий
-		"PARENT_SECTION" => "",	// ID раздела
-		"PARENT_SECTION_CODE" => "",	// Код раздела
-		"PREVIEW_TRUNCATE_LEN" => "",	// Максимальная длина анонса для вывода (только для типа текст)
-		"PROPERTY_CODE" => array(	// Свойства
-			0 => "",
-			1 => "",
-		),
-		"SET_BROWSER_TITLE" => "N",	// Устанавливать заголовок окна браузера
-		"SET_LAST_MODIFIED" => "N",	// Устанавливать в заголовках ответа время модификации страницы
-		"SET_META_DESCRIPTION" => "N",	// Устанавливать описание страницы
-		"SET_META_KEYWORDS" => "N",	// Устанавливать ключевые слова страницы
-		"SET_STATUS_404" => "N",	// Устанавливать статус 404
-		"SET_TITLE" => "N",	// Устанавливать заголовок страницы
-		"SHOW_404" => "N",	// Показ специальной страницы
-		"SORT_BY1" => "ACTIVE_FROM",	// Поле для первой сортировки новостей
-		"SORT_BY2" => "SORT",	// Поле для второй сортировки новостей
-		"SORT_ORDER1" => "DESC",	// Направление для первой сортировки новостей
-		"SORT_ORDER2" => "ASC",	// Направление для второй сортировки новостей
-		"STRICT_SECTION_CHECK" => "N",	// Строгая проверка раздела для показа списка
-	),
-	false
-);?>
- </section><? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6">
+                <div class="who-we">
+                    <h2 class="title-1"><? $APPLICATION->IncludeComponent(
+                                            "bitrix:main.include",
+                                            "",
+                                            array(
+                                                "AREA_FILE_RECURSIVE" => "Y",
+                                                "AREA_FILE_SHOW" => "file",
+                                                "AREA_FILE_SUFFIX" => "inc",
+                                                "EDIT_TEMPLATE" => "standard.php",
+                                                "PATH" => SITE_TEMPLATE_PATH . "/include_file/servises/titel.php"
+                                            )
+                                        ); ?></h2>
+                    <p>
+                        <? $APPLICATION->IncludeComponent(
+                            "bitrix:main.include",
+                            "",
+                            array(
+                                "AREA_FILE_RECURSIVE" => "Y",
+                                "AREA_FILE_SHOW" => "file",
+                                "AREA_FILE_SUFFIX" => "inc",
+                                "EDIT_TEMPLATE" => "standard.php",
+                                "PATH" => SITE_TEMPLATE_PATH . "/include_file/servises/text1.php"
+                            )
+                        ); ?>
+                    </p>
+                    <p>
+                        <? $APPLICATION->IncludeComponent(
+                            "bitrix:main.include",
+                            "",
+                            array(
+                                "AREA_FILE_RECURSIVE" => "Y",
+                                "AREA_FILE_SHOW" => "file",
+                                "AREA_FILE_SUFFIX" => "inc",
+                                "EDIT_TEMPLATE" => "standard.php",
+                                "PATH" => SITE_TEMPLATE_PATH . "/include_file/servises/text2.php"
+                            )
+                        ); ?>
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6">
+                <div class="our-skill">
+                    <h2 class="title-1"><? $APPLICATION->IncludeComponent(
+                                            "bitrix:main.include",
+                                            "",
+                                            array(
+                                                "AREA_FILE_RECURSIVE" => "Y",
+                                                "AREA_FILE_SHOW" => "file",
+                                                "AREA_FILE_SUFFIX" => "inc",
+                                                "EDIT_TEMPLATE" => "standard.php",
+                                                "PATH" => SITE_TEMPLATE_PATH . "/include_file/servises/name_skill.php"
+                                            )
+                                        ); ?>
+                    </h2>
+                    <? $APPLICATION->IncludeComponent(
+        "bitrix:news.list",
+        "servises_skill_list",
+        array(
+            "ACTIVE_DATE_FORMAT" => "d.m.Y",    // Формат показа даты
+            "ADD_SECTIONS_CHAIN" => "N",    // Включать раздел в цепочку навигации
+            "AJAX_MODE" => "N",    // Включить режим AJAX
+            "AJAX_OPTION_ADDITIONAL" => "",    // Дополнительный идентификатор
+            "AJAX_OPTION_HISTORY" => "N",    // Включить эмуляцию навигации браузера
+            "AJAX_OPTION_JUMP" => "N",    // Включить прокрутку к началу компонента
+            "AJAX_OPTION_STYLE" => "N",    // Включить подгрузку стилей
+            "CACHE_FILTER" => "N",    // Кешировать при установленном фильтре
+            "CACHE_GROUPS" => "N",    // Учитывать права доступа
+            "CACHE_TIME" => "36000000",    // Время кеширования (сек.)
+            "CACHE_TYPE" => "N",    // Тип кеширования
+            "CHECK_DATES" => "Y",    // Показывать только активные на данный момент элементы
+            "DETAIL_URL" => "",    // URL страницы детального просмотра (по умолчанию - из настроек инфоблока)
+            "DISPLAY_BOTTOM_PAGER" => "N",    // Выводить под списком
+            "DISPLAY_DATE" => "N",    // Выводить дату элемента
+            "DISPLAY_NAME" => "Y",    // Выводить название элемента
+            "DISPLAY_PICTURE" => "N",    // Выводить изображение для анонса
+            "DISPLAY_PREVIEW_TEXT" => "Y",    // Выводить текст анонса
+            "DISPLAY_TOP_PAGER" => "N",    // Выводить над списком
+            "FIELD_CODE" => array(    // Поля
+                0 => "NAME",
+                1 => "PREVIEW_TEXT",
+                2 => "",
+            ),
+            "FILTER_NAME" => "",    // Фильтр
+            "HIDE_LINK_WHEN_NO_DETAIL" => "N",    // Скрывать ссылку, если нет детального описания
+            "IBLOCK_ID" => "7",    // Код информационного блока
+            "IBLOCK_TYPE" => "content",    // Тип информационного блока (используется только для проверки)
+            "INCLUDE_IBLOCK_INTO_CHAIN" => "N",    // Включать инфоблок в цепочку навигации
+            "INCLUDE_SUBSECTIONS" => "N",    // Показывать элементы подразделов раздела
+            "MESSAGE_404" => "",    // Сообщение для показа (по умолчанию из компонента)
+            "NEWS_COUNT" => "5",    // Количество новостей на странице
+            "PAGER_BASE_LINK_ENABLE" => "N",    // Включить обработку ссылок
+            "PAGER_DESC_NUMBERING" => "N",    // Использовать обратную навигацию
+            "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",    // Время кеширования страниц для обратной навигации
+            "PAGER_SHOW_ALL" => "N",    // Показывать ссылку "Все"
+            "PAGER_SHOW_ALWAYS" => "N",    // Выводить всегда
+            "PAGER_TEMPLATE" => ".default",    // Шаблон постраничной навигации
+            "PAGER_TITLE" => "Новости",    // Название категорий
+            "PARENT_SECTION" => "",    // ID раздела
+            "PARENT_SECTION_CODE" => "",    // Код раздела
+            "PREVIEW_TRUNCATE_LEN" => "",    // Максимальная длина анонса для вывода (только для типа текст)
+            "PROPERTY_CODE" => array(    // Свойства
+                0 => "",
+                1 => "",
+            ),
+            "SET_BROWSER_TITLE" => "N",    // Устанавливать заголовок окна браузера
+            "SET_LAST_MODIFIED" => "N",    // Устанавливать в заголовках ответа время модификации страницы
+            "SET_META_DESCRIPTION" => "N",    // Устанавливать описание страницы
+            "SET_META_KEYWORDS" => "N",    // Устанавливать ключевые слова страницы
+            "SET_STATUS_404" => "N",    // Устанавливать статус 404
+            "SET_TITLE" => "N",    // Устанавливать заголовок страницы
+            "SHOW_404" => "N",    // Показ специальной страницы
+            "SORT_BY1" => "ACTIVE_FROM",    // Поле для первой сортировки новостей
+            "SORT_BY2" => "SORT",    // Поле для второй сортировки новостей
+            "SORT_ORDER1" => "DESC",    // Направление для первой сортировки новостей
+            "SORT_ORDER2" => "ASC",    // Направление для второй сортировки новостей
+            "STRICT_SECTION_CHECK" => "N",    // Строгая проверка раздела для показа списка
+        ),
+        false
+    ); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
