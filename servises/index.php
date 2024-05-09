99d31194cf770d382aaa084e11ee7495e4f6883c
Роман Карнаухов <WorkKarn@yandex.ru>
2024-05-09 12:49:04 +0700

    Создание включаемых облостей и начало листа со скилами

--- servises/index.php
+++ servises/index.php
@@ -1,8 +1,8 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Услуги");
+$APPLICATION->SetPageProperty('TITLE', "Услуги");
+$APPLICATION->SetTitle("ЧЕМ МЫ ЗАНИМАЕМСЯ");
 ?>
 
-Text here....
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
